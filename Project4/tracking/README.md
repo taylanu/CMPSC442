41b626c22b3300dacd793691534e4144c5c99054
Taylan <taylanu@users.noreply.github.com>
2021-04-08 15:43:37 -0400

    Update README.md

---
+++ Project4/tracking/README.md
@@ -0,0 +1,8 @@
+## Project4 CMPSC442
+## Due: April 25th @ Midnight
+
+## Commands to Run:
+python busters.py
+
+### Run Single Test
+python autograder.py -t test_cases/q1/1-ObsProb
--- Project4/tracking/inference.py
+++ Project4/tracking/inference.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -348,7 +348,7 @@ class ParticleFilter(InferenceModule):
         Return the agent's current belief state, a distribution over ghost
         locations conditioned on all evidence and time passage. This method
         essentially converts a list of particles into a belief distribution.
-        
+
         This function should return a normalized distribution.
         """
         "*** YOUR CODE HERE ***"
